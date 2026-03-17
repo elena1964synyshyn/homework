a2789a597b04226764da4a0574164bfe9f9964aa
elena <synyshyn1@gmail.com>
2026-03-17 20:54:55 +0100

    created index.html and update script.js

---
+++ js/script.js
@@ -0,0 +1,62 @@
+// 1
+// let name = "Olena";
+// let sity = "Napoli";
+// city = name;
+// console.log(city);
+
+
+// 2
+let name = "Olena";
+// console.log(`привіт ${1}`);привіт 1
+// console.log(`привіт ${"name"}`);привіт name
+// console.log(`привіт ${name}`);привіт Olena
+
+// 3
+// let a = "5";
+// let b = "13cvb";
+// let c = "12.9sxdcfgv";
+// console.log(typeof Number(a));-number
+// console.log(typeof parseInt(b));-number
+// console.log(typeof parseFloat(c));-number
+
+// 4
+// console.log((0.1 * 10 + 0.2 * 10) /10);-0,3
+
+// 5
+// console.log(Math.max(20, 10, 50, 40));-50
+
+//***6**
+// console.log(Math.random() * (4 - 2) + 2);=2.7802751375150363
+
+//***7**
+// const message = "Welcome to Bahamas!";
+// console.log(message.length);-19
+
+//***8**
+// const message = "Welcome to Bahamas!";
+// console.log(message.toUpperCase());-WELCOME TO BAHAMAS!
+
+//***9**
+// let user = {};
+// user.name = " Olena";
+// user.age = 61;
+// user.city = "Napoli";
+// // console.log(user);
+// delete user.city;
+// user['like flowers'] = true;
+// console.log(user);
+// -{ name: ' Olena', age: 61, city: 'Napoli' }
+//  {name: ' Olena', age: 61, like flowers: true}
+
+//***10**
+
+// for (key in user) {
+//   console.log(key);
+//   console.log(user[key]);
+// }
+// -name
+// script.js:55  Olena
+// script.js:54 age
+// script.js:55 61
+// script.js:54 like flowers
+// script.js:55 true
