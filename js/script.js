020df141eefeab083fdcc3f2126f79f4d8df2b44
elena <synyshyn1@gmail.com>
2026-03-20 21:49:25 +0100

    update scripn.js

--- js/script.js
+++ js/script.js
@@ -24,18 +24,18 @@
 // 5
 // console.log(Math.max(20, 10, 50, 40));-50
 
-//***6**
+// ***6**
 // console.log(Math.random() * (4 - 2) + 2);=2.7802751375150363
 
-//***7**
+// ***7**
 // const message = "Welcome to Bahamas!";
 // console.log(message.length);-19
 
-//***8**
+// ***8**
 // const message = "Welcome to Bahamas!";
 // console.log(message.toUpperCase());-WELCOME TO BAHAMAS!
 
-//***9**
+// ***9**
 // let user = {};
 // user.name = 'Olena';
 // user.age = 61;
@@ -47,7 +47,7 @@
 // -{ name: ' Olena', age: 61, city: 'Napoli' }
 //  {name: ' Olena', age: 61, like flowers: true}
 
-//***10**
+// ***10**
 
 // for (key in user) {
 //   console.log(key);
@@ -63,11 +63,15 @@
 // const message = 'Javascript is awesome!';
 
 // // виведення
+
 // alert(message);
 // console.log(message);
 
+// #2 lesson
+
 // Отримання даних
-// confirm
+
+// Confirm
 // const isconfirm = confirm('Please confirm reservation');
 // console.log(isconfirm);
 // -true;
@@ -75,9 +79,112 @@
 
 // console.log(test);-true;
 
-// confirm
+// Confirm
+
 // const isConfirm = confirm('Please cofirm reservation');
 // console.log(isconfirm);
 
-const test = confirm('Yes or no?');
-console.log = test;
+// const test = confirm('Yes or no?');
+// console.log = test;
+
+// Prompt
+
+// const userName = prompt('Pleas enter your name:');
+// console.log(userName);
+// const number = prompt('Pleas enter your number:');
+// console.log(number);
+
+// String
+// value = false;
+// console.log(typeof value);
+
+// const newValue = String(value);
+// console.log(typeof newValue);
+
+// /number
+
+// value = '123';
+// console.log(typeof value);
+// const newValue = Number(value);
+// console.log(typeof newValue);
+
+// Boolean
+
+// console.log(Boolean(1));
+// console.log(Boolean(0));
+
+// console.log(Boolean('Вітаю'));
+// console.log(Boolean(''));
+
+// console.log(Boolean(NaN));
+// console.log(Boolean(null));
+
+// Оператори
+// console.log(2 + 2);
+// console.log(10 - 4);
+// console.log(3 * 5);
+// console.log(15 / 3);
+
+// Остачы выд дылення
+
+// console.log(8 % 3);
+// console.log(10 % 3);
+// console.log(10 % 2);
+
+// Пыднесення до степеня
+
+// console.log(5 ** 2);
+// console.log(4 ** 4);
+
+// Оператори порывняння
+// a==b-дорівнює
+// А!=в не дорівнює
+// а===в строго дорівнює
+// а!==в строго не дорівнює
+
+// console.log(5 > 3);
+// console.log(5 < 3);
+// console.log(5 >= 5);
+// console.log(7 <= 4);
+
+// let value = 0;
+// console.log(value == 0);
+
+// Приведення до типу
+// and
+
+// console.log(5 === 5 && 6 > 3);
+// console.log(6 + 3 >= 9 && 8 + 1 === 9);
+
+// or-(abo)
+
+// console.log(5 + 5 === 25 || 2 * 2 === 6);
+
+// not
+
+// console.log(знак оклику означае false);
+
+// if-якщо
+
+// if (5 + 5 === 10){
+//   console.log("Hello");
+// }
+// if (5 + 5 === 25){console.log("1");}else{
+//   console.log("2");
+// }
+
+// if (2 + 2 === 4){
+//   console.log("Hello");
+// }else{console.log("No hello");}
+
+// Тернарний оператор
+// 2+2===4? console.log("1") : console.log("2")
+// 5*5===24 ? console.log(true) : console.log(false);
+
+if (5+5 === 7){
+  console.log("5+5 === 7");
+}else if(2+2 === 4){
+  console.log("2+2 === 4");
+}else{
+  console.log("no");
+} 
