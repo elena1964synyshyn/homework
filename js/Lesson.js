b976ed1f82af53efa3f7aa927b0ba511e3cd9158
elena <synyshyn1@gmail.com>
2026-04-07 21:58:38 +0200

    update html and js

--- js/Lesson.js
+++ js/Lesson.js
@@ -1,5 +1,7 @@
 // Модуль № 24
 
+// const { jsxs } = require('react/jsx-runtime');
+
 // Функції
 
 // function showMessage (name, sity) {
@@ -578,3 +580,62 @@
 // for (let elem of elements) {
 //   alert(elem.innerHTML); // "тест", "пройдено"
 // }
+
+// Modul # 30
+// _______________________________________________
+
+// localStorage.setItem('name', 'Olena');
+// const a = localStorage.getItem('name');
+// console.log(a);
+
+// function check() {
+//   let val = document.getElementsByName('radio');
+//   if (val[0].checked) {
+//     console.log('Жіноча');
+//   } else {
+//     console.log('Чоловіча');
+//   }
+// }
+
+// let form = document.querySelector('.form');
+// let nameInput = document.querySelector('.inputName');
+// let LastNameInput = document.querySelector('.inputLastName');
+// let age = document.querySelector('#age');
+// let male = document.querySelector('radio');
+
+// form.addEventListener('submit', handleSubmit);
+// function handleSubmit(event) {
+//   event.preventDefault();
+//   const info = {
+//     name: nameInput.value,
+//     lastName: LastNameInput.value,
+//     age: age.value,
+
+//     male: document.querySelector('input[value="male"]').checked,
+//   };
+//   console.log(info);
+//   localStorage.setItem('info', JSON.stringify(info));
+// }
+
+// if (localStorage.getItem('info')) {
+//   const ls = JSON.parse(localStorage.getItem('info'));
+//   console.log(ls);
+//   nameInput.value = ls.name;
+//   LastNameInput.value = ls.lastName;
+// }
+
+let form = document.querySelector('#form');
+let formData = {};
+
+form.addEventListener('input', function (event) {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem('formData', JSON.stringify(formData));
+});
+
+if (localStorage.getItem('formData')) {
+  formData = JSON.parse(localStorage.getItem('formData'));
+
+  for (let key in formData) {
+    form.elements[key].value = formData[key];
+  }
+}
